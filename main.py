227d727cc18342097409af85f42dda16d9ab73bd
Serkan Ozel <serkan.ozel@boun.edu.tr>
2019-05-27 12:18:50 +0300

    first running code, data adjustment, splitting and training can be done

--- main.py
+++ main.py
@@ -1,10 +1,77 @@
-import keras
+from keras.layers import Dense, Dropout, Activation, Conv2D, MaxPooling2D, Flatten
+from keras.models import Sequential
+from keras.utils import to_categorical
+from keras.optimizers import SGD
+
 import cv2
 import subprocess 
 import os 
+import numpy as np
 
-animal_folders_list = ['bear', 'cougar', 'cow', 'coyote', 'deer', 'elephant', 'giraffe', 'goat', 'gorilla', 'horse', 'kangaroo', 'leopard', 'lion', 'panda', 'penquin', 'sheep', 'skunk', 'tiger', 'zebra']
+animal_folders_list = [
+    'bear',
+    'cougar',
+    'cow',
+    'coyote',
+    'deer',
+    'elephant',
+    'giraffe',
+    'goat',
+    'gorilla',
+    'horse',
+    'kangaroo',
+    'leopard',
+    'lion',
+    'panda',
+    'penquin',
+    'sheep',
+    'skunk', 
+    'tiger',
+    'zebra'
+]
+classname_to_class_number = {
+    'bear' : 0,
+    'cougar' : 1,
+    'cow' : 2,
+    'coyote': 3,
+    'deer': 4,
+    'elephant' : 5 ,
+    'giraffe' : 6,
+    'goat' : 7,
+    'gorilla' : 8,
+    'horse' : 9,
+    'kangaroo' : 10,
+    'leopard' : 11,
+    'lion' : 12,
+    'panda' : 13,
+    'penquin' : 14,
+    'sheep' : 15,
+    'skunk' : 16, 
+    'tiger' : 17,
+    'zebra' : 18
+}
 
+class_number_to_classname = {
+    0 :'bear',
+    1 :'cougar',
+    2 :'cow',
+    3 :'coyote',
+    4 :'deer',
+    5 :'elephant',
+    6 :'giraffe',
+    7 :'goat',
+    8 :'gorilla',
+    9 :'horse',
+    10 :'kangaroo',
+    11 :'leopard',
+    12 :'lion',
+    13 :'panda',
+    14 :'penquin',
+    15 :'sheep',
+    16 :'skunk', 
+    17 :'tiger',
+    18 :'zebra'
+}
 animal_images = {} # keys: animal names, values: array of images 
 
 for animal_name in animal_folders_list:  # remove trailing whitespaces after animal names
@@ -23,12 +90,91 @@ print("------------DATASET SUMMARY-------------")
 for animal in animal_images: # print dataset summary
     print(animal+" : "+str(len(animal_images[animal])))
 
-for animal in animal_images: # resize images as (300,300,3)
+for animal in animal_images: # preprocessing: resize images as (300,300,3)
     for x in range(len(animal_images[animal])):
         animal_images[animal][x] = cv2.resize(animal_images[animal][x],(300,300))
 
 batch_size = 32
 num_classes = 19
-epochs = 100
-save_dir = os.path.join(os.getcwd(), 'saved_models')
-model_name = 'cmpe462_kth_animals.h5'
+epochs = 11
+#save_dir = os.path.join(os.getcwd(), 'saved_models')
+#model_name = 'cmpe462_kth_animals.h5'
+
+training_set = {}
+validation_set = {}
+test_set = {}
+
+# splitting the dataset into training, validation and test set with 80,10,10 ratio.
+# first find 10 percent(int division), allocate that amount to test and val sets from beginning of the dataset
+# and then the rest is training set.
+for animal in animal_images:
+    ten_percent = len(animal_images[animal])//10
+    test_set[animal] = animal_images[animal][0:ten_percent]
+    validation_set[animal] = animal_images[animal][ten_percent+1:ten_percent*2+1]
+    training_set[animal] = animal_images[animal][ten_percent*2+1:]
+
+training_set_as_array = []
+training_labels_as_array = []
+
+for classname in training_set:
+    for animal in training_set[classname]:
+        training_set_as_array.append(animal)
+        training_labels_as_array.append(classname_to_class_number[classname])
+
+validation_set_as_array = []
+validation_labels_as_array = []
+
+for classname in validation_set:
+    for animal in validation_set[classname]:
+        validation_set_as_array.append(animal)
+        validation_labels_as_array.append(classname_to_class_number[classname])
+
+test_set_as_array = []
+test_labels_as_array = []
+
+for classname in test_set:
+    for animal in test_set[classname]:
+        test_set_as_array.append(animal)
+        test_labels_as_array.append(classname_to_class_number[classname])
+
+# Convert to numpy array for keras 
+training_set_as_array  = np.array(training_set_as_array)
+training_labels_as_array  = np.array(training_labels_as_array)
+validation_set_as_array  = np.array(validation_set_as_array)
+validation_labels_as_array  = np.array(validation_labels_as_array)
+test_set_as_array  = np.array(test_set_as_array)
+test_labels_as_array  = np.array(test_labels_as_array)
+
+
+# Convert to one hot 
+
+training_labels_as_array = to_categorical(training_labels_as_array,19)
+validation_labels_as_array = to_categorical(validation_labels_as_array,19)
+test_labels_as_array = to_categorical(test_labels_as_array,19)
+
+model = Sequential()
+model.add(Conv2D(64, (6,6) , input_shape=(300,300,3),activation="relu"))
+model.add(MaxPooling2D(pool_size=(4, 4)))
+
+model.add(Conv2D(64, (6,6) , input_shape=(300,300,3),activation="relu"))
+model.add(MaxPooling2D(pool_size=(4, 4)))
+
+model.add(Conv2D(128, (6,6) , input_shape=(300,300,3),activation="relu"))
+model.add(MaxPooling2D(pool_size=(4, 4)))
+model.add(Flatten())
+
+model.add(Dense(128,activation="relu"))
+model.add(Dropout(0.5))
+
+model.add(Dense(19, activation='softmax'))
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+
+model.compile(optimizer=sgd,
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+
+
+model.fit(training_set_as_array,training_labels_as_array,epochs=epochs,validation_data=(validation_set_as_array,validation_labels_as_array))
+
+score = model.evaluate(test_set_as_array, test_labels_as_array)
